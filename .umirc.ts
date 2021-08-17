dc276ce1844e2f08c12fc46908e8fe35b04f04b8
xiaoze77 <470532446@qq.com>
2021-08-17 11:41:28 +0800

    fix: 修改文字

--- .umirc.ts
+++ .umirc.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   navs: [
     null, // null 值代表保留约定式生成的导航，只做增量配置
     {
-      title: '赞助',
+      title: '打赏',
       path: 'https://dun.mianbaoduo.com/@xiaoze77',
     },
     {
