ef7bca0c5e66075c2673e7825cf893588d435e86
xz <med@meddeMacBook-Air.local>
2022-01-27 16:19:05 +0800

    fix: 修改一些名称

--- .umirc.ts
+++ .umirc.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'dumi';
 
 export default defineConfig({
-  title: 'xiaoze',
+  title: 'xz',
   favicon: '/assets/seven.png',
   logo: '/assets/seven.png',
   outputPath: 'docs-dist',
@@ -10,13 +10,9 @@ export default defineConfig({
   // 单语言配置方式如下
   navs: [
     null, // null 值代表保留约定式生成的导航，只做增量配置
-    // {
-    //   title: '打赏',
-    //   path: 'https://dun.mianbaoduo.com/@xiaoze77',
-    // },
     {
       title: 'GitHub',
-      path: 'https://github.com/xiaoze77/xiaoze77.github.io',
+      path: 'https://github.com/xz-77/xz-77.github.io',
     },
   ],
 });
